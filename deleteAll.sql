6ac5be312611c489f3205af80737f6276e682a52
mqprichard <mqprichard@gmail>
2013-08-07 00:27:43 -0700

    Created project

---
+++ deleteAll.sql
@@ -0,0 +1,8 @@
+use gasp-db;
+delete from review;
+alter table review auto_increment = 1;
+delete from restaurant;
+alter table restaurant auto_increment = 1;
+delete from user;
+alter table user auto_increment = 1;
+
